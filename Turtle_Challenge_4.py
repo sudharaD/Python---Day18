c731ad9d555167076e648155bb1cdf21b95b31f8
sudharaD <sudharad7@gmail.com>
2023-01-16 22:25:29 +0530

    challenge 4 finish

--- Turtle_Challenge_4.py
+++ Turtle_Challenge_4.py
@@ -4,13 +4,18 @@ class Challenge4:
 
     timothy_the_turtle = t.Turtle()
     timothy_the_turtle.shape("turtle")
+    timothy_the_turtle.pensize(5)
     color_list = ["deep sky blue", "spring green", "yellow", "red", "magenta", "black"]
-    angles = [90, 180, 270, 360]
+    angles = [0, 90, 180, 270]
 
-    def random_walk(self, distance):
-        self.timothy_the_turtle.right(self.choice(self.angles))
-        self.timothy_the_turtle.forward(distance)
-        self.timothy_the_turtle.color(self.choice(self.color_list))
+    screen = t.Screen()
+    screen.delay(1)
+
+    def random_walk(self, distance, loops):
+        for _ in range(loops):
+            self.timothy_the_turtle.setheading(self.choice(self.angles))
+            self.timothy_the_turtle.forward(distance)
+            self.timothy_the_turtle.pencolor(self.choice(self.color_list))
 
 
 
